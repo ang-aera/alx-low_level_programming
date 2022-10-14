cff572d40e63e6290dd57fe8dd64cc823ba58400
ang-aera <angellamaera@gmail.com>
2022-10-14 14:06:22 -0700

    Update 5.0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry Point
  *
  * Return: Always 0 (Success)
  */
@@ -9,6 +9,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	fprintf(stderr, "");
 	return (0);
 }
