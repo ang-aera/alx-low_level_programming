51a03dac01bec520762eb77df30c3fb780d2e85d
ang-aera <angellamaera@gmail.com>
2022-10-14 13:55:00 -0700

    Update 3.0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	fprintf(stderr, "HELP!");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	fprintf(stderr, "[Anything]");
 	return (0);
 }
