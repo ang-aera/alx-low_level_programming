1def7a3902b3dccf5982d1ee6f9fdb47c4a6180a
ang-aera <angellamaera@gmail.com>
2022-10-18 19:19:15 +0000

    Created file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - program that prints alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
