68b1032a258d6b37ceee5dd847d26a5be7fb576f
ang-aera <angellamaera@gmail.com>
2022-10-19 11:37:27 +0000

    Created file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	char letter1;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	putchar(letter);
+
+	for (letter1 = 'A'; letter1 <= 'Z'; letter1++)
+
+	putchar(letter1);
+
+	putchar('\n');
+
+	return (0);
+
+
+
+}
